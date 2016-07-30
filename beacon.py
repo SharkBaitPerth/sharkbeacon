f2f333cff74eff17f095b10e0f28d3c5821ac2ae
Leon Wright <leon.wright@imdexlimited.com>
2016-07-30 14:03:49 +0800

    MQTT server

--- beacon.py
+++ beacon.py
@@ -26,7 +26,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("", 1883, 60)
+client.connect("52.62.201.125", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
